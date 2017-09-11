40b15d0d81fba2ece930d1e7eff97120cf6284bf
alexlao <lao.alex97@gmail.com>
2017-09-10 22:41:22 -0400

    Script now properly handles posts and stores information into database.

--- serverScripts/getRide.php
+++ serverScripts/getRide.php
@@ -1,13 +1,44 @@
 <?php
 # $json = array('response'=>'Hello this is a test');
+$servername = "localhost";
+$username = "alexaqj1_admin";
+$pw = "Al3xL4oz!!";
+$dbname = "alexaqj1_quickRide";
+
+$connection = new mysqli($servername, $username, $pw, $dbname);
+if($connection->connect_error){
+	die("Connection failed: " . $connection->connect_error);
+}
 
 if($_SERVER['REQUEST_METHOD']==='GET'){
 	$json = array('response' => 'response from get');
 	echo json_encode($json);
 }
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-	$json = array('response' =>'Hi '.  $_POST["name"]); 
+	# $sql = "INSERT INTO Rides(name, address, group) VALUES ($_POST["name"], $_POST["address"], $_POST["group"])";
+	if(!($stmt = $connection->prepare("INSERT INTO Rides (name, address, userGroup) VALUES (?, ?, ?)"))){
+		error_log("Prepare Failed: " . $connection->errno . ") " . $connection->error);
+	}
+	$stmt->bind_param("sss", $name, $address, $group);
+	$name = $_POST["name"];
+	$address = $_POST["address"];
+	$group = $_POST["userGroup"];
+	if($stmt->execute()){
+		$json = array('response' => 'success');	
+	} else{
+		$json = array('response' => 'failed');	
+	}
+	/*
+	if($connection->query($sql) === TRUE){
+		echo "new record was submitted";
+	}
+	else{
+		echo "Error: " . $connection->error;
+	}*/
+	$stmt->close();
 	echo json_encode($json);
 }
+
+$connection->close();
 
 ?>
